e01b54c38d7fb2b0727126782e583344ef4cee58
Diegoh <diego.hdez@hotmail.com>
2021-01-19 19:53:30 -0600

    holamundoi
    se creo escena y cambio al debug log

---
+++ Assets/Scripts/holamundo2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holamundo2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int i = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log("hola "+i);
+    }
+}
